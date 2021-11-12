f13f1f627adad2e01559790cde8c1bf2fd2ab1c7
Murat <example@gmail.com>
2021-11-12 21:25:19 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+// let interestRate = 0.3;
+// interestRate = 1;
+// console.log(interestRate)
+
+// let name = 'Mosh';
+// let age = 30
+
+// let person = {
+//     name: 'Mosh',
+//     age: 30
+// };
+
+// person.name = 'John';
+
+
+// let selection = 'name';
+
+// person[selection] = 'Mary';
+
+// console.log(person.name);
+
+
+
+// let isApproved = false;
+// let firstName = undefined;
+// let selected = null;
+
+
+
+// let selectedColors = ['red', 'blue'];
+
+// selectedColors[2]=1;
+// console.log(selectedColors.length);
+
+
+function greet(name, lastname) {
+    console.log('Hello ' + name +' '+ lastname);
+}
+
+function square(number) {
+    return number*number;
+}
+let number = square(2);
+console.log(number);
+
+greet('Murat');
+
+greet('Mary');
