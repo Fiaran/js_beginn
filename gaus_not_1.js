ba6d512f1b52cbd19d79ee1e63b55caae8a1fda1
Murat <example@gmail.com>
2021-12-03 18:56:30 +0600

    index.html mod

---
+++ gaus_not_1.js
@@ -0,0 +1,24 @@
+function name(na, nb) {
+    let s1=0;
+    let s2=0;
+
+    while(na/(Math.pow(10,1))>=1){
+        s1=s1+1;
+    }
+    while(nb/(Math.pow(10,1))>=1){
+        s2=s2+1;
+    }
+    if(d1>d2){
+        d=d1;
+    }
+    else{
+        d=d2;
+    }
+    return d;
+  }
+
+function multiplyy(n1, n2 ,d){
+    if (d==1){
+        return n1*n2;
+    }
+}
